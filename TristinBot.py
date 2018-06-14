e4712dc7684731cd01e3713181e5ff39d0601d2c
Tristo <15713784+TristoKrempita@users.noreply.github.com>
2018-06-14 02:12:23 +0200

    Added dictionary, reply and eBay
    You can now reply to messages by using the new !reply message_id "Your message"
    Now introcuding dictionaries!
    You can now add an item into the dictionary by using the !dictionary_add command and let the public decide on weather to put it in or not. The voting system has a 1 vote per person limitation and doesn't stand in the way of regular commands being executed.  
    Once an item has been introduced into the dictionary you can print the whole thing or search by keyword(s)
    You can now search for your favorite stuff on eBay using the !ebay "Thing i want" lower_price_limit high_price_limit and get the cheapest item on the list including free shipping!

--- TristinBot.py
+++ TristinBot.py
@@ -15,6 +15,20 @@ ch = open('channels.txt').read().splitlines()
 risk = ch[0]
 retar = ch[1]
 daily = ch[2]
+
+dict = {'domebijuk':'To bail on a plan minutes from meeting people that are meters away',
+'LMOA':'Laughing My Obese Ass',
+'4?':'Are you up for a(multiple) game(s) of Fortnite?'}
+
+dictLines = open('dictBoy.txt').read().splitlines()
+keyLine = ""
+valueLine = ""
+for line in dictLines:
+	if(len(line.split('|'))>1):
+		keyLine = line.split('|')[0]
+		valueLine = line.split('|')[1]
+	dict[keyLine] = valueLine
+
 #initialising Reddit user
 reddit = praw.Reddit(client_id='CLIENT ID',client_secret='CLIENT SECRET',user_agent='USER AGENT')
 
@@ -24,15 +38,6 @@ imgclient_secret = 'CLIENT SECRET'
 imgclient = ImgurClient(imgclient_id, imgclient_secret)
 
 client = Bot(description="Najbolji bot north of the wall", command_prefix="!", pm_help = False, )
-
-# @client.event
-# async def my_background_task():
-# 	print("yo")
-# 	line = open('twitter.txt').read()
-# 	with open('twitter.txt', 'w') as f:
-# 		f.write('')
-# 	if len(line)!=0:
-# 		await client.send_message(client.get_channel('417039633122328606'),line)
 
 async def my_background_task():
 	await client.wait_until_ready()
@@ -47,7 +52,15 @@ async def my_background_task():
 
 @client.event
 async def on_ready():
-
+	global votes_up
+	global votes_down
+	global msgToDelete
+	global msgToAddToDict
+	global msgToCompare
+	global voteOnce
+	voteOnce = []
+	votes_up = 0
+	votes_down = 0
 	print("Bot is ready!")
 	return await client.change_presence(game=discord.Game(name='Still being developed'))
 
@@ -325,6 +338,202 @@ async def bleep(*args):
 	""" Writes "bloop" within 15 minutes. Syntax : !bleep """
 	await asyncio.sleep(secrets.randbelow(900))
 	await client.say("bloop")
+
+
+@client.command()
+async def ebay(*args):
+	"""Gets ebay listings. Syntax:!red cup 1 5 | Will get cheapest cup between 1$ and 5$"""
+	userSearch = args
+	url = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw="
+	#userSearch = userSearch.split(' ')
+	for search in userSearch[:-1]:
+	    url += search+"+"
+	url = url[:-1]
+	r_html = requests.get(url).text
+	soup = BeautifulSoup(r_html,"html.parser")
+	chr = soup.find_all('h3',class_="s-item__title")
+	cijene = soup.find_all('span',class_="s-item__price")
+	links = soup.find_all('a',class_="s-item__link")
+	cijeneFinal = []
+	for i in range(0,len(cijene)):
+	    #if the parent is the same it means there are 2 or more prices in the same item category and we don't want them to clog up the cijene[] and cause
+	    #some items to have wrong prices attached to them so we check for that and remove any prices but the base one
+	    if((cijene[i].parent.parent.parent.parent.parent == cijene[i-1].parent.parent.parent.parent.parent)):
+	        continue
+	    else:
+	        cijeneFinal.append(cijene[i])
+	values = [len(chr),len(cijeneFinal),len(links)]
+	#print(values)
+	filterCijena = int(str(userSearch[-1]))
+	filterCijenaMin = int(str(userSearch[-2]))
+	#print(">"+str(filterCijena))
+	class item:
+	    ime = None
+	    cijena = None
+	    cijenaString = None
+	    link = None
+	    parentTag = None
+
+	replaced = ""
+	items = []
+	itemsFinal=[]
+	#return this after you fix the bid 2 prices on 1 item being treated as 2 prices on 2 items
+	for graf in range(min(values)):
+	    items.append(item())
+	for i in range(0,min(values)):
+	    #assigns title text to the ime variable of object
+	    if(chr[i].text[0:11].lower() == "new listing"):
+	        items[i].ime=chr[i].text[11:]
+	    else:
+	        items[i].ime = chr[i].text
+	    items[i].cijenaString = cijeneFinal[i].text
+	    #turning the string we got from the price in the $3.45 format into a 3.45 float
+	    if(any(char.isdigit() for char in str(cijeneFinal[i].string))):
+	        #if the price has a ',' replace all '.' with '' and all ',' with '.' and remove last 2 digits (the decimal remainder)
+	        if(',' in str(cijeneFinal[i].string)[1:]):
+	            replaced = str(cijeneFinal[i].string)[1:].replace(".","")
+	            replaced = replaced[:-2]
+	            replaced = replaced.replace(",",".")
+	            items[i].cijena = float(replaced)
+	        #if the price doesn't have a ',' juts remove the '$' sign and store it as a float inside the item class
+	        else:
+	            items[i].cijena = float(str(cijeneFinal[i].string)[1:])
+	    #if the price isn't a price at all we set the value to the first decimal of the first number ($0.13 to $13.5) => 0.1
+	    else:
+	        items[i].cijena = float(str(cijeneFinal[i].text)[1:4])
+	        #print("|||"+str(float(str(cijeneFinal[i].text)[1:4])))
+	    #we assign the href memeber of the tag to the link variable of object
+	    items[i].link = links[i]['href']
+	    items[i].parentTag = chr[i].parent.parent.parent
+
+	#sorting the array of objects by attribute cijena
+	items.sort(key=lambda x: x.cijena, reverse=False)
+
+	#makes a new list of items and stores the ones that have free shipping in it
+	freeShipping = []
+	for i in range(0,len(items)):
+	    ship = items[i].parentTag.find_all("span",class_="s-item__shipping s-item__logisticsCost")
+	    for s in ship:
+	        if(s.text=="Free Shipping"):
+	            items[i].ime= "***FREE SHIPPING : *** " +items[i].ime
+	            freeShipping.append(items[i])
+
+	#merges freeShipping with items
+	for x in items:
+	    if(x in freeShipping):
+	        continue
+	    else:
+	        freeShipping.append(x)
+	br=0
+	discordBr = 0
+	#prints list of items until upper limit
+	for x in freeShipping:
+		if(x.cijena != None and x.cijena<filterCijena and x.cijena> filterCijenaMin and x.cijena != -0.5 and discordBr<1):
+			await client.say(str(x.ime)+" | "+str(x.cijenaString)+" | "+str(x.link)+"\n")
+			discordBr+=1
+
+
+
+
+@client.command()
+async def what(*args):
+	"""Lookup a meaning of word Syntax : !what _word_ """
+	str1 = ' '.join(args)
+	if(str1 in dict):
+		await client.say(str(dict[str1]))
+
+
+@client.command()
+async def dictionary(*args):
+	"""Lists all entries in the dictionary"""
+	#await client.say(dict.values()[0])
+	for a in dict.keys():
+		if(a == ""):
+			continue
+		await client.say(a + " -> " + dict[a])
+anotherEntry = 0
+@client.command()
+async def dictionary_add(*args):
+	"""Syntax: !dictionary_add word blahblah definition blah blah"""
+	global msgToAddToDict
+	global msgToDelete
+	global msgToCompare
+	key=""
+	value=""
+	try:
+		start_key =args.index("word")
+		start_value=args.index("definition")
+	except ValueError:
+		await client.say("No _word_ or _definition_ in syntax")
+		start_value=0
+		start_key=0
+	for i in range(start_key+1,start_value):
+		key+=args[i]
+		key+=' '
+	for i in range(start_value+1,len(args)):
+		value+=args[i]
+		value+=' '
+	with open('tempDictBoy.txt','w') as file:
+		file.write(key+"|"+value)
+	msgToCompare = f"Vote with 👍 / 👎 to add/not add this to the dictionary: {key}|{value}"
+	msgToAddToDict = key+" | "+value
+	msgToDelete = await client.say(msgToCompare)
+
+
+@client.event
+async def on_reaction_add(reaction, user):
+	global votes_up
+	global votes_down
+	global msgToDelete
+	global msgToAddToDict
+	global msgToCompare
+	global voteOnce
+	dictionaryString = ""
+	dictionaryList = []
+	if((reaction.emoji == '👍' and reaction.message.content+" " == msgToCompare) and user.id not in voteOnce):
+		voteOnce.append(user.id)
+		votes_up+=1
+	elif((reaction.emoji == '👎' and reaction.message.content+" " == msgToCompare) and user.id not in voteOnce):
+		voteOnce.append(user.id)
+		votes_down+=1
+
+	if(votes_up > 2):
+		await client.send_message(msgToDelete.channel,f"The entry \"{msgToAddToDict}\" has been added to the dictionary")
+		await client.delete_message(msgToDelete)
+		with open('tempDictBoy.txt','r') as file:
+			dictionaryString = file.read()
+		#dictionaryList = dictionaryString.split('|')
+		with open('dictBoy.txt','a') as file:
+			file.write(dictionaryString + "\n")
+		votes_up = 0
+		votes_down = 0
+		voteOnce = []
+	elif(votes_down > 2):
+		await client.send_message(msgToDelete.channel,f"The entry \"{msgToAddToDict}\" was not added to the dictionary")
+		await client.delete_message(msgToDelete)
+		votes_up = 0
+		votes_down = 0
+		voteOnce = []
+	dictLines = open('dictBoy.txt').read().splitlines()
+	keyLine = ""
+	valueLine = ""
+	for line in dictLines:
+		if(len(line.split('|'))>1):
+			keyLine = line.split('|')[0]
+			valueLine = line.split('|')[1]
+		dict[keyLine] = valueLine
+
+
+
+@client.command(pass_context=True)
+async def reply(ctx,*args):
+	"""Syntax : !reply message_ID Your response"""
+	msg = await client.get_message(ctx.message.channel, args[0])
+	await client.say(f" ```{msg.author.name} : {msg.content}``` "+ctx.message.author.name+" : "+' '.join(args[1:]))
+	await client.delete_message(ctx.message)
+
+
+
 
 client.loop.create_task(my_background_task())
 client.run('BOT TOKEN')
